892e13cb09599996669c62cdd9a0de0aed4d529e
EC2 Default User <ec2-user@ip-172-31-91-207.ec2.internal>
2022-04-11 13:32:44 +0000

    meshiterro2_220411

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 #空のモデルでは、PostImage モデルで定義したカラムを@post_image.user_idのように、"[モデル名].[カラム名]"という形で繋げることで、保存するカラムの中身を操作することができる。
   resources :post_images, only:[:new, :create, :index, :show, :destroy]
 #userコントローラ用のルーティングの記述
-  resources :users, only:[:show, :edit]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only:[:show, :edit, :update]
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
