b0b7b0cb7d7a0d8f1af8adcb9e6e09ae9ba63b98
EC2 Default User <ec2-user@ip-172-31-91-207.ec2.internal>
2022-04-14 14:30:38 +0000

    meshiterro_220414

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
 #今回の場合、new,create,index,show以外のルーティングは行われない
 #理由としては、画像投稿の機能では「新規投稿(new)」「一覧(index)」「詳細機能(show)」「削除(destroy)」しか必要ない為
 #空のモデルでは、PostImage モデルで定義したカラムを@post_image.user_idのように、"[モデル名].[カラム名]"という形で繋げることで、保存するカラムの中身を操作することができる。
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resource :favorites, only:[:create, :destroy]
+    resources :post_comments, only:[:create, :destroy]
+  end
 #userコントローラ用のルーティングの記述
   resources :users, only:[:show, :edit, :update]
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
